b17ac6e0addb91b979e96dd72d58cd4558969927
Illarion Medvedev <illarion2000@inbox.lv>
2017-07-31 22:17:09 +0100

    now with sticky div

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-    include "db.php";
-
-    $query = mysqli_query( $con,"SELECT * FROM days");
-    $posts = array();
+include "db.php";
+$query = mysqli_query( $con,"SELECT * FROM days");
+$posts = array();
 
 if($query) {
     while($row = mysqli_fetch_assoc($query)){
@@ -24,29 +23,32 @@ if($query) {
 </head>
 <body>
   <div class="container">
-      <h1>My project history</h1>
+     <section>
+      <h1 class="sticky">My project history</h1>
       <p>Current time: <span class="toolbar"></span> </p>
-          <ul>
-              <?php foreach($posts as $post):?>
-                 <li>
-                        <span></span>
-                        <div class="row advanced animated slideInRight">
-                        <div class="col-md-6">
-                        <h3><?= $post['name']?></h3>
-                        <div class="title"><b>Title:</b> <?= $post['title']?></div>
-                        <div class="info"><b>Info:</b> <?= $post['info']?></div>
-                        <div class="type"><b>Type:</b> <?= $post['type']?></div>
-                        </div>
-                        <div class="col-md-6"><img src="images/q.jpg" class="img-fluid"></div>
-                        </div>
-                        <div class="time">
-                             <span><?= $post['time_begin']?></span>
-                             <span><?= $post['time_end']?></span>
-                        </div>
-                 </li>
-              <?php endforeach ?>
-          </ul>
+     <ul>
+        <?php foreach($posts as $post):?>
+         <li>
+            <span></span>
+            <div class="row advanced animated slideInRight">
+            <div class="col-md-6">
+             <h3><?= $post['name']?></h3>
+             <div class="title"><b>Title:</b> <?= $post['title']?></div>
+             <div class="info"><b>Info:</b> <?= $post['info']?></div>
+             <div class="type"><b>Type:</b> <?= $post['type']?></div>
+             </div>
+             <div class="col-md-6"><img src="images/q.jpg" class="img-fluid"></div>
+             </div>
+             <div class="time">
+                 <span><?= $post['time_begin']?></span>
+                 <span><?= $post['time_end']?></span>
+             </div>
+         </li>
+         <?php endforeach ?>
+     </ul>
+      </section>
   </div>
+  <script src="js/app.js"></script>
   <script src="js/jquery-3.2.1.min%20(1).js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script>
